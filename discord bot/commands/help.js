4858bcb1583a5ebc1b6d98297e280ee8044fc018
Dayana <124521128+Dayana3333@users.noreply.github.com>
2026-03-18 18:01:15 +0100

    asd

--- discord bot/commands/help.js
+++ discord bot/commands/help.js
@@ -12,18 +12,11 @@ module.exports = {
         `ℹ️ **.help** - Kiírja az elérhető parancsokat.\n` +
         `📖 **.szab** - Szerver szabályzat kíirása. \n` +
         `😀 **.emojilist** - Megjeleníti a jelenlegi emojikat a szerveren. \n\n` +
-        /*`**.start** Bot elinditása.\n` +
-        `**.reload** Bot újrainditása.\n` +
-        `**.stop** Bot leállitása. \n` +*/
         `+ *A botban található egy **spam gátló rendszer** is*.` +
         `+ *AntiRaid*` 
       )
       .setFooter({ text: `Parancsot kérte: ${message.author.tag}` })
-      .setAuthor({
-        name: 'Készítő: regedit_404', // A bot fejlesztője
-        iconURL: 'https://media.discordapp.net/attachments/1410384904104972401/1412875523910012958/E8CAB3A7-8579-43C4-B02A-EDA91AC67894.png'
-      })
-      .setTimestamp();
+     .setTimestamp();
 
     await message.channel.send({ embeds: [embed] });
   }
